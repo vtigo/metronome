bba11e00810be4a8ad6824e0ab9e6496cfacf59a
vtigo <rf.tigas@gmail.com>
2026-05-29 00:05:03 -0300

    change movement speed on bpm

--- main.c
+++ main.c
@@ -1,13 +1,11 @@
 #include <raylib.h>
+
 #define CORNFLOWERBLUE (Color){ 100, 149, 237 }
+
 #define WINDOW_WIDTH  800
 #define WINDOW_HEIGHT 500
-#define LERP_SPEED    4.0f
-#define BEAT_COUNT    4
 
-typedef struct {
-    int bpm;
-} Metronome;
+#define BEAT_COUNT    4
 
 int main(void)
 {
@@ -15,14 +13,15 @@ int main(void)
     InitAudioDevice();
     Sound tick_sound = LoadSound("tick.mp3");
 
-    Metronome metronome = { .bpm = 60 };
-    double beat_interval = 60.0 / metronome.bpm;
-
+    int bpm = 60;
+    double beat_interval = 60.0 / bpm;
+    int current_beat = 0;
+    
+    // Used to transform the center thing
     float wide = 100.0f, narrow = 10.0f;
     float cur_w = wide, cur_h = narrow;
     float tgt_w = narrow, tgt_h = wide;
 
-    int current_beat = 0;
 
     SetTargetFPS(60);
 
@@ -31,7 +30,8 @@ int main(void)
     while (!WindowShouldClose())
     {
         float dt = GetFrameTime();
-
+        
+        // Beat time
         if (GetTime() - time_begin >= beat_interval) {
             PlaySound(tick_sound);
             time_begin = GetTime();
@@ -42,9 +42,11 @@ int main(void)
 
             current_beat = (current_beat + 1) % BEAT_COUNT;
         }
-
-        cur_w += (tgt_w - cur_w) * LERP_SPEED * dt;
-        cur_h += (tgt_h - cur_h) * LERP_SPEED * dt;
+        
+        // Center animation
+        float anim_time = bpm * 0.1f;
+        cur_w += (tgt_w - cur_w) * anim_time * dt;
+        cur_h += (tgt_h - cur_h) * anim_time * dt;
 
         Rectangle arm = {
             .x      = WINDOW_WIDTH  / 2.0f - cur_w / 2.0f,
@@ -53,47 +55,50 @@ int main(void)
             .height = cur_h,
         };
 
-        if (IsKeyPressed(KEY_UP) && metronome.bpm < 300) {
-            metronome.bpm ++;
-            beat_interval = 60.0 / metronome.bpm;
+        if (IsKeyPressed(KEY_UP) && bpm < 300) {
+            bpm ++;
+            beat_interval = 60.0 / bpm;
         }
-        if (IsKeyPressed(KEY_DOWN) && metronome.bpm > 30) {
-            metronome.bpm --;
-            beat_interval = 60.0 / metronome.bpm;
+        if (IsKeyPressed(KEY_DOWN) && bpm > 30) {
+            bpm --;
+            beat_interval = 60.0 / bpm;
         }
         if (IsKeyPressed(KEY_RIGHT)) {
-            if (metronome.bpm >= 290) {
-                metronome.bpm = 300;
+            if (bpm >= 290) {
+                bpm = 300;
             } else {
-                metronome.bpm += 10;
+                bpm += 10;
             }
-            beat_interval = 60.0 / metronome.bpm;
+            beat_interval = 60.0 / bpm;
         }
-        if (IsKeyPressed(KEY_LEFT) && metronome.bpm > 30) {
-            if (metronome.bpm <= 40) {
-                metronome.bpm = 30;
+        if (IsKeyPressed(KEY_LEFT) && bpm > 30) {
+            if (bpm <= 40) {
+                bpm = 30;
             } else {
-                metronome.bpm -= 10;
+                bpm -= 10;
             }
-            beat_interval = 60.0 / metronome.bpm;
+            beat_interval = 60.0 / bpm;
         }
 
         BeginDrawing();
             ClearBackground(CORNFLOWERBLUE);
-
-            const char *bpm_text = TextFormat("%i", metronome.bpm);
+            
+            // BPM display
+            const char *bpm_text = TextFormat("%i", bpm);
             int text_width = MeasureText(bpm_text, 60);
             DrawText(bpm_text, WINDOW_WIDTH / 2 - text_width / 2, 100, 60, WHITE);
-
+            
+            // Center thing display
             DrawRectangleRec(arm, WHITE);
-
-            int   circle_r  = 10;
-            int   dash_w    = 20;
-            int   dash_h    = 4;
-            int   spacing   = 40;
-            int   total_w   = BEAT_COUNT * dash_w + (BEAT_COUNT - 1) * spacing;
-            int   start_x   = WINDOW_WIDTH / 2 - total_w / 2;
-            int   indicator_y = 400;
+            
+            // Beat counts display
+            int circle_r = 10;
+            int dash_w   = 20;
+            int dash_h   = 4;
+            int spacing  = 40;
+            int total_w  = BEAT_COUNT * dash_w + (BEAT_COUNT - 1) * spacing;
+            int start_x  = WINDOW_WIDTH / 2 - total_w / 2;
+            int indicator_y = 400;
 
             for (int i = 0; i < BEAT_COUNT; i++) {
                 int cx = start_x + i * (dash_w + spacing) + dash_w / 2;
